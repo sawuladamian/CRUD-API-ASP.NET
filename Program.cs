42834b81aae4fba770311bbc0cbc5e542d55e101
Damian Sawuła <damian.sawula@gmail.com>
2022-02-18 12:18:37 +0100

    MongoDb Repository included

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 using Catalog.Repositories;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using Catalog.Settings;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+    BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+    BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+builder.Services.AddSingleton<IMongoClient>(serviceProvider => 
+    {
+     var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+     return new MongoClient(settings.ConnectionString);
+    });
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
